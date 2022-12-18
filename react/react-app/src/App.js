ab5fb3d4cc126937da71b141c3d57ee87dc2b284
Harimad <leesh68481@gmail.com>
2022-12-19 00:46:06 +0900

    react-redux ToDo 실습

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import Detail from './components/redux/Detail'
+import Home from './components/redux/Home'
 
-const App = () => {
-  return <div>App</div>
+function App() {
+  return (
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
 export default App
