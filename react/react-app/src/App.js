8985a7677a6fff9a8b026c86fbb922481f754b8e
Harimad <leesh68481@gmail.com>
2022-12-18 22:56:59 +0900

    file arranged

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,58 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { db } from './firebase'
-import { collection, doc, getDocs, setDoc } from 'firebase/firestore'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './components/1212Problem/Root'
-import Home from './components/1212Problem/Home'
-import Error from './components/1212Problem/Error'
-import Products from './components/1212Problem/Products'
-import Product from './components/1212Problem/Product'
+import React from 'react'
 
 const App = () => {
-  const fruitCollection = collection(db, 'fruits')
-  const [fruits, setFruits] = useState([])
-
-  // Firebase 데이터 조회
-  useEffect(() => {
-    async function getFruits() {
-      const data = await getDocs(fruitCollection)
-      setFruits(data.docs)
-    }
-    getFruits()
-  }, [])
-
-  // 수정
-  async function makeFruit(fruit) {
-    await setDoc(doc(fruitCollection), {
-      name: fruit.name,
-      season: fruit.season,
-      color: fruit.color,
-      taste: fruit.taste,
-      count: fruit.count,
-      price: fruit.price,
-    })
-  }
-
-  // 라우터 정의
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: <Home makeFruit={makeFruit} />,
-        },
-        {
-          path: '/products',
-          element: <Products fruits={fruits} />,
-        },
-        { path: '/products/:productId', element: <Product /> },
-      ],
-    },
-  ])
-
-  return <RouterProvider router={router} />
+  return <div>App</div>
 }
+
 export default App
