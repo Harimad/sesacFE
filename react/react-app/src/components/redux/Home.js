6dc1a42464ff00477864bd9d20ccfcdd0d08476e
Harimad <leesh68481@gmail.com>
2022-12-19 01:05:35 +0900

    redux-toolkit ToDo 실습

--- react/react-app/src/components/redux/Home.js
+++ react/react-app/src/components/redux/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import ToDo from './ToDo'
-import { actionCreators } from './store'
+import { add } from './store'
 
 function Home({ toDos, addToDo }) {
   // 이제 Home에서 dispatch 함수를 사용할 수 있다.
@@ -44,14 +44,13 @@ function Home({ toDos, addToDo }) {
 // mapStateToProps를 사용한다는 의미는 redux store로부터 니가 무엇인가를 가져오고 싶다는 의미이다.
 // 가져오고 나서 props에 넣는다. 어디에? component의 props에..
 // mapStateToProps로부터 주는 state는 react-router에 의해서 home의 props가 된다.
-
 function getCurrentState(state) {
   return { toDos: state } // Home props에 들어간당! 무엇을 리턴해주든 home의 prop로 들어간다.
 }
 
 // 여기서 props를 바꿀 수 있는 파워가 생겼당~~
 function mapDispatchToProps(dispatch) {
-  return { addToDo: text => dispatch(actionCreators.addToDo(text)) }
+  return { addToDo: text => dispatch(add(text)) }
 }
 
 // getCurrentState를 사용해서 Home에게 store로부터 state를 가져다 줄 것이다.
